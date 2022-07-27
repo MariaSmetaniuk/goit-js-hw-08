58f34ef73cafcb44902c55a5bfaf65156627265f
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-07-27 12:06:55 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+
+player.on('timeupdate', throttle(onTimeupdateInLocalStorage, 1000));
+
+const currentTime = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(currentTime);
+
+function onTimeupdateInLocalStorage(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}
